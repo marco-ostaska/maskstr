9566301582f8b78c58f3f11d9e55b4b113b4097c
Marco Ostaska <marcoan@br.ibm.com>
2018-11-19 18:37:42 -0200

    maskstr 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# stamaskstr
+# maskstr
 
 ## Hash
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stamaskstr",
+    name="maskstr",
     version="0.0.1",
     author="Marco Ostaska",
     author_email="marcoan@ymail.com",
